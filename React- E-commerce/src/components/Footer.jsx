8043471e7e9323c4bc422084ff109ca35b64c076
Rinku Maurya <rinkubhai2143200@gmail.com>
2024-03-05 14:34:53 +0530

    Add Initial Files

---
+++ React- E-commerce/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="mb-0 text-center">
+        <div className="d-flex align-items-center justify-content-center pb-5">
+          <div className="col-md-6">
+            <p className="mb-3 mb-md-0">Made with ❤️ by Rinku Maurya </p>
+            <a className="text-dark fs-4" href="https://github.com/rinkumaurya23" target="_blank" rel="noreferrer">
+              <i className="fa fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
